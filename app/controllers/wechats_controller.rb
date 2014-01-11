43b7e1a6a749594b4b431979142e9f7f91b301a6
liu liang <liul85@gmail.com>
2014-01-11 11:58:50 +0800

    add controller wechats and finish check_signature connected with weixin server

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -1,5 +1,17 @@
 class WechatsController < ApplicationController
+  before_filter :check_signature
+
   def show
+    render :text => params[:echostr]
+  end
+
+  def create
     
   end
+
+  private
+    def check_signature
+      array = ['liuliang', params[:timestamp], params[:nonce]].sort
+      render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
+    end
 end
