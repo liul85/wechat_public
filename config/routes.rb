0b20266111320d8e90999de01603b42f4470aa91
liu liang <liul85@gmail.com>
2014-01-28 17:57:41 +0800

    finish static_pages and twitter-bootstrap-rails style

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 WechatPublic::Application.routes.draw do
-  resource :wechat
+  resources :wechat
+  root to: 'static_pages#index'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+#  match '/signup', to: 'static_pages#sign_up', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
